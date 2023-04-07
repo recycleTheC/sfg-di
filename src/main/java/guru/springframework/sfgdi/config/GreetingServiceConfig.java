2359af3f588121460fedecbfde62929fc0118abf
recycleTheC <mario.kopjar1@gmail.com>
2023-04-07 13:26:13 +0200

    Using Properties Source with Profiles

--- src/main/java/guru/springframework/sfgdi/config/GreetingServiceConfig.java
+++ src/main/java/guru/springframework/sfgdi/config/GreetingServiceConfig.java
@@ -11,7 +11,6 @@ import org.springframework.context.annotation.*;
 
 @Configuration
 @ImportResource("classpath:sfgdi-config.xml")
-@PropertySource("classpath:datasource.properties")
 public class GreetingServiceConfig {
 
     @Bean
